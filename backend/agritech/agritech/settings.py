5edd1dba9b0653445f7c2e5d0336ddf3ea76bdcc
Emmanuel Michira <emmanuelmichira@gmail.com>
2020-10-20 20:52:01 +0300

    insall django-cors-headers update requirements.txt file

--- backend/agritech/agritech/settings.py
+++ backend/agritech/agritech/settings.py
@@ -37,9 +37,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'rest_framework',
+
+    #My Apps
+    'api.apps.ApiConfig',
 ]
 
+CORS_ALLOW_ALL_ORIGINS = True
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
