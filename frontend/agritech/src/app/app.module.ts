bdb1accd877b363dfa0f53d63d94057c2fdf7ff2
emichira <emmanuelmichira@gmail.com>
2021-06-18 16:17:50 +0300

    build static home-page template create feature and pages directory folders add header, footer and home-page component add images

--- frontend/agritech/src/app/app.module.ts
+++ frontend/agritech/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './features/header/header.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { FooterComponent } from './features/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomePageComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
